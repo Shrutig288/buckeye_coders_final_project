7a27115650a8b6aa0466be22a4f41fcfcfd73200
Shruti Garg <garg.209@osu.edu>
2025-12-03 15:13:51 -0500

    Fixed the creating new trips

---
+++ db/migrate/20251203201209_create_trips.rb
@@ -0,0 +1,12 @@
+class CreateTrips < ActiveRecord::Migration[8.1]
+  def change
+    create_table :trips do |t|
+      t.string :name
+      t.date :start_date
+      t.date :end_date
+      t.text :participants
+
+      t.timestamps
+    end
+  end
+end
