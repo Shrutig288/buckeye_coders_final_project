f2774904cf5bd2dbd77b7c0db452794a95cc5933
Shruti Garg <garg.209@osu.edu>
2025-12-03 15:22:01 -0500

    Finished creating new trip

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,70 +1,28 @@
 class TripsController < ApplicationController
-  before_action :set_trip, only: %i[ show edit update destroy ]
-
-  # GET /trips or /trips.json
   def index
     @trips = Trip.all
   end
 
-  # GET /trips/1 or /trips/1.json
   def show
+    @trip = Trip.find(params[:id])
   end
 
-  # GET /trips/new
   def new
     @trip = Trip.new
   end
 
-  # GET /trips/1/edit
-  def edit
-  end
-
-  # POST /trips or /trips.json
   def create
     @trip = Trip.new(trip_params)
-
-    respond_to do |format|
-      if @trip.save
-        format.html { redirect_to @trip, notice: "Trip was successfully created." }
-        format.json { render :show, status: :created, location: @trip }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @trip.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /trips/1 or /trips/1.json
-  def update
-    respond_to do |format|
-      if @trip.update(trip_params)
-        format.html { redirect_to @trip, notice: "Trip was successfully updated.", status: :see_other }
-        format.json { render :show, status: :ok, location: @trip }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @trip.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /trips/1 or /trips/1.json
-  def destroy
-    @trip.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to trips_path, notice: "Trip was successfully destroyed.", status: :see_other }
-      format.json { head :no_content }
+    if @trip.save
+      redirect_to @trip
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_trip
-      @trip = Trip.find(params.expect(:id))
-    end
 
-    # Only allow a list of trusted parameters through.
-    def trip_params
-      params.expect(trip: [ :name, :start_date, :end_date, :participants ])
-    end
+  def trip_params
+    params.require(:trip).permit(:name, :start_date, :end_date, :participants)
+  end
 end
